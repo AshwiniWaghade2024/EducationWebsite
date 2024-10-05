a555246403012e96bb2c8b3858d263dc787c7d93
ASHWINI WAGHADE <158551421+AshwiniWaghade2024@users.noreply.github.com>
2024-10-05 20:30:37 +0530

    Create Head.jsx

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'D:/my_code/my-job-grow/my-app/src/App.css'
+
+const Head = () => {
+    return (
+        <>
+            <section className='head'>
+                <div className='container flexSB'>
+                    <div className="logo">
+                        <h1>ACADEMEIA</h1>
+                        <span>ONLINE EDUCATION & LEARNING</span>
+                    </div>
+
+                    <div className='social'>
+                        <i className='fab fa-facebook-f icon'></i>
+                        <i className='fab fa-instagram icon'></i>
+                        <i className='fab fa-twitter icon'></i>
+                        <i className='fab fa-youtube icon'></i> 
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Head
